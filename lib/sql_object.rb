04ef8e9c6045acf7990454e54589723aa4f6bac7
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-09-05 17:48:45 -0400

    Complete method for columns and finalize to create getter and setter for each column

--- lib/sql_object.rb
+++ lib/sql_object.rb
@@ -9,8 +9,35 @@ class SQLObject
   end
 
   def self.table_name
-    @table_name || self.to_s.tableize
+    @table_name || to_s.tableize
   end
 
+  def self.columns
+    if @cols.nil?
+      @cols = DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{table_name}
+        LIMIT
+          1
+      SQL
+    end
+    @cols.first.map(&:to_sym)
+  end
 
+  def self.finalize!
+    columns.each do |col|
+      set_col = "#{col}="
+
+      define_method(col) do
+        attributes[col]
+      end
+
+      define_method(set_col) do |value|
+        attributes[col] = value
+      end
+    end
+  end
+  
 end
